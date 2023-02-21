3a95aa1fefffac8f26a31e2c8900c2a52c1a9181
Teng Jiang <jiangteng0403@gmail.com>
2023-02-21 15:52:56 -0500

    Update download_finetuned.sh
    No need to mkdir $data_dir.

--- download_finetuned.sh
+++ download_finetuned.sh
@@ -3,6 +3,5 @@ data_dir="pretrained_spanbert"
 model="tacred"
 echo Downloading pre-trained SpanBERT 
 wget -P $data_dir http://dl.fbaipublicfiles.com/fairseq/models/spanbert_$model.tar.gz
-mkdir $data_dir
 tar xvzf $data_dir/spanbert_$model.tar.gz -C $data_dir
 rm $data_dir/spanbert_$model.tar.gz
